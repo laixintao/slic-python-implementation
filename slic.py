fa7702c1edbfbc8496bacbca910577d50c25309a
赖信涛 <laixintao1995@163.com>
2017-03-12 23:11:04 +0800

    refactor with class

--- slic.py
+++ slic.py
@@ -1,138 +1,167 @@
-
 import math
 from skimage import io, color
 import numpy as np
+from tqdm import trange
 
-def open_image(path):
-    '''
-    Return:
-        3D array, row col [LAB]
-    '''
-    rgb = io.imread(path)
-    lab_arr = color.rgb2lab(rgb)
-    return lab_arr
-
-def save_lab_image(path, lab_arr):
-    '''Convert the aray to RBG, then save the image'''
-    rgb_arr = color.lab2rgb(lab_arr)
-    io.imsave(path, rgb_arr)
 
 class Cluster(object):
-    def __init__(self,x,y,l=0,a=0,b=0):
-        self.update(x,y,l,a,b)
+    cluster_index = 1
 
-    def update(self,x,y,l,a,b):
-        self.x = x
-        self.y = y
+    def __init__(self, h, w, l=0, a=0, b=0):
+        self.update(h, w, l, a, b)
+        self.pixels = []
+        self.no = self.cluster_index
+        self.cluster_index += 1
+
+    def update(self, h, w, l, a, b):
+        self.h = h
+        self.w = w
         self.l = l
         self.a = a
         self.b = b
 
     def __str__(self):
-        return "{},{}:{} {} {} ".format(self.x, self.y, self.l, self.a, self.b)
+        return "{},{}:{} {} {} ".format(self.h, self.w, self.l, self.a, self.b)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class SLICProcessor(object):
+    @staticmethod
+    def open_image(path):
+        """
+        Return:
+            3D array, row col [LAB]
+        """
+        rgb = io.imread(path)
+        lab_arr = color.rgb2lab(rgb)
+        return lab_arr
+
+    @staticmethod
+    def save_lab_image(path, lab_arr):
+        """
+        Convert the array to RBG, then save the image
+        :param path:
+        :param lab_arr:
+        :return:
+        """
+        rgb_arr = color.lab2rgb(lab_arr)
+        io.imsave(path, rgb_arr)
+
+    def make_cluster(self, h, w):
+        return Cluster(h, w,
+                       self.data[h][w][0],
+                       self.data[h][w][1],
+                       self.data[h][w][2])
+
+    def __init__(self, filename, K):
+        self.K = K
+        self.M = 40
+
+        self.data = self.open_image(filename)
+        self.image_height = self.data.shape[0]
+        self.image_width = self.data.shape[1]
+        self.N = self.image_height * self.image_width
+        self.S = int(math.sqrt(self.N / self.K))
+
+        self.clusters = []
+        self.label = {}
+        self.dis = np.full((self.image_height, self.image_width), np.inf)
+
+    def init_clusters(self):
+        h = self.S / 2
+        w = self.S / 2
+        while h < self.image_height:
+            while w < self.image_width:
+                self.clusters.append(self.make_cluster(h, w))
+                w += self.S
+            w = self.S / 2
+            h += self.S
+
+    def get_gradient(self, h, w):
+        if w + 1 >= self.image_width:
+            w = self.image_width - 2
+        if h + 1 >= self.image_height:
+            h = self.image_height - 2
+
+        gradient = self.data[w + 1][h + 1][0] - self.data[w][h][0] + \
+                   self.data[w + 1][h + 1][1] - self.data[w][h][1] + \
+                   self.data[w + 1][h + 1][2] - self.data[w][h][2]
+        return gradient
+
+    def move_clusters(self):
+        for cluster in self.clusters:
+            cluster_gradient = self.get_gradient(cluster.h, cluster.w)
+            for dh in range(-1, 2):
+                for dw in range(-1, 2):
+                    _h = cluster.h + dh
+                    _w = cluster.w + dw
+                    new_gradient = self.get_gradient(_h, _w)
+                    if new_gradient < cluster_gradient:
+                        cluster.update(_h, _w, self.data[_h][_w][0], self.data[_h][_w][1], self.data[_h][_w][2])
+                        cluster_gradient = new_gradient
+
+    def assignment(self):
+        for cluster in self.clusters:
+            for h in range(cluster.h - 2 * self.S, cluster.h + 2 * self.S):
+                if h < 0 or h >= self.image_height: continue
+                for w in range(cluster.w - 2 * self.S, cluster.w + 2 * self.S):
+                    if w < 0 or w >= self.image_width: continue
+                    L, A, B = self.data[h][w]
+                    Dc = math.sqrt(
+                        math.pow(L - cluster.l, 2) +
+                        math.pow(A - cluster.a, 2) +
+                        math.pow(B - cluster.b, 2))
+                    Ds = math.sqrt(
+                        math.pow(h - cluster.h, 2) +
+                        math.pow(w - cluster.w, 2))
+                    D = math.sqrt(math.pow(Dc / self.M, 2) + math.pow(Ds / self.S, 2))
+                    if D < self.dis[h][w]:
+                        if (h, w) not in self.label:
+                            self.label[(h, w)] = cluster
+                            cluster.pixels.append((h, w))
+                        else:
+                            self.label[(h, w)].pixels.remove((h, w))
+                            self.label[(h, w)] = cluster
+                            cluster.pixels.append((h, w))
+                        self.dis[h][w] = D
+
+    def update_cluster(self):
+        for cluster in self.clusters:
+            sum_h = sum_w = number = 0
+            for p in cluster.pixels:
+                sum_h += p[0]
+                sum_w += p[1]
+                number += 1
+                _h = sum_h / number
+                _w = sum_w / number
+                cluster.update(_h, _w, self.data[_h][_w][0], self.data[_h][_w][1], self.data[_h][_w][2])
+
+    def save_current_image(self, name):
+        image_arr = np.copy(self.data)
+        for cluster in self.clusters:
+            for p in cluster.pixels:
+                image_arr[p[0]][p[1]][0] = cluster.l
+                image_arr[p[0]][p[1]][1] = cluster.a
+                image_arr[p[0]][p[1]][2] = cluster.b
+            image_arr[cluster.h][cluster.w][0] = 0
+            image_arr[cluster.h][cluster.w][1] = 0
+            image_arr[cluster.h][cluster.w][2] = 0
+        self.save_lab_image(name, image_arr)
+
+    def iterate_10times(self):
+        self.init_clusters()
+        self.move_clusters()
+        for i in trange(10):
+            self.assignment()
+            name = 'slic_{}_assignment_M{}_K{}.png'.format(i, self.M, self.K)
+            self.save_current_image(name)
+            self.update_cluster()
+            name = 'slic_{}_update_M{}_K{}.png'.format(i, self.M, self.K)
+            self.save_current_image(name)
 
 
 if __name__ == '__main__':
-    image_arr = open_image('lenna.png')
-    image_height = image_arr.shape[0]
-    image_width = image_arr.shape[1]
-
-    K = 2000
-    N = image_width * image_height
-    label = np.zeros((image_width, image_height))
-    dis = np.full((image_width, image_height), np.inf)
-
-    C = []
-    print 'N:', N
-
-    # Initialization
-    S = int(math.sqrt(N/K)) # S the distance
-    print 'S: ',S
-    i = S/2
-    j = S/2
-    while i<image_height:
-        while j<image_width:
-            C.append(Cluster(i,j,image_arr[i][j][0],image_arr[i][j][1],image_arr[i][j][2]))
-            j+=S
-        j=S/2
-        i+=S
-    print 'len(C):', len(C)
-
-    def get_gradient(x,y):
-        if x+1>=image_width:
-            x = image_width - 2
-        if y+1>=image_height:
-            y = image_height - 2
-
-        gradient = image_arr[x+1][y+1][0] - image_arr[x][y][0] + \
-                   image_arr[x+1][y+1][1] - image_arr[x][y][1] + \
-                   image_arr[x+1][y+1][2] - image_arr[x][y][2]
-        return gradient
-
-    # re-choose a point in 3x3
-    for point in C:
-        point_gradient = get_gradient(point.x, point.y)
-        for i in range(-1,2):
-            for j in range(-1,2):
-                x = point.x + i
-                y = point.y + j
-                new_gradient = get_gradient(x,y)
-                if new_gradient < point_gradient:
-                    point_gradient = new_gradient
-                    point.update(x,y,image_arr[x][y][0],image_arr[x][y][1],image_arr[x][y][2])
-
-    # Assignment
-    print 'Assignment...'
-    for point in C:
-        print point
-        for i in range(point.x-2*S, point.x+2*S):
-            if i<0 or i>=image_width:
-                continue
-            for j in range(point.y-2*S, point.y+2*S):
-                if j<0 or j>=image_height:
-                    continue
-                L,A,B = image_arr[i][j]
-                m = 30
-                Dc = math.sqrt(
-                        math.pow(L-point.l,2)+
-                        math.pow(A-point.a,2)+
-                        math.pow(B-point.b,2))
-                Ds = math.sqrt(
-                        math.pow(i-point.x,2)+
-                        math.pow(j-point.y,2))
-                D = math.pow(Dc/m,2) + math.pow(Ds/S,2)
-                D = math.sqrt(D)
-                if D < dis[i][j]:
-                    label[i][j] = C.index(point)
-                    dis[i][j] = D
-
-    print 'Update...'
-    for point in C:
-        print point
-        point_index = C.index(point)
-        pixels = []
-        sum_x = sum_y =  number = 0
-        for i in range(image_width):
-            for j in range(image_height):
-                if int(label[i][j]) == point_index:
-                    sum_x += i
-                    sum_y += j 
-                    number += 1
-        avg_x = sum_x/number
-        avg_y = sum_y/number
-        point.update(avg_x, avg_y, image_arr[avg_x][avg_y][0],image_arr[avg_x][avg_y][1],image_arr[avg_x][avg_y][2])
-
-
-    for i in range(0,image_width):
-        for j in range(0,image_height):
-            point = C[int(label[i][j])]
-            image_arr[i][j][0] = point.l
-            image_arr[i][j][1] = point.a
-            image_arr[i][j][2] = point.b
-
-    for point in C:
-        image_arr[point.x][point.y][0] = 0
-        image_arr[point.x][point.y][1] = 0
-        image_arr[point.x][point.y][2] = 0
-
-    save_lab_image('arr.png', image_arr)
+    p = SLICProcessor('Lenna.png', 200)
+    p.iterate_10times()
